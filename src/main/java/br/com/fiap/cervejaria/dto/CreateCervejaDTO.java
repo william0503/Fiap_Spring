329a33460cba02deb8f12122239a9f1623f3dffd
William Rocha <william0503@gmail.com>
2020-01-23 22:22:49 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/fiap/cervejaria/dto/CreateCervejaDTO.java
@@ -0,0 +1,52 @@
+package br.com.fiap.cervejaria.dto;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public class CreateCervejaDTO {
+    private String marca;
+    private Double teorAlcoolico;
+    private Tipo tipo;
+    private BigDecimal preco;
+    private ZonedDateTime dataLancamento;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getTeorAlcoolico() {
+        return teorAlcoolico;
+    }
+
+    public void setTeorAlcoolico(Double teorAlcoolico) {
+        this.teorAlcoolico = teorAlcoolico;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public ZonedDateTime getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(ZonedDateTime dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+}
